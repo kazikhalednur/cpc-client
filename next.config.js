72fcad5e3babdfa4754ae1f6fb7de53047e82754
Kazi Khaled Nur <kazikhalednur@gmail.com>
2025-10-17 12:03:32 +0600

    add image patterns

--- next.config.js
+++ next.config.js
@@ -9,6 +9,7 @@ const nextConfig = {
       { protocol: "https", hostname: "res.cloudinary.com" },
       { protocol: "https", hostname: "storage.googleapis.com" },
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
+      { protocol: "https", hostname: "cpc-server-59ee74ed57a0.herokuapp.com" },
     ],
   },
 };
